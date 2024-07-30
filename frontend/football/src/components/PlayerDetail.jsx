c01c54fd2ad43fa047ef7dc8ed7a1a23039fd11e
jslempa <jslempa@gmail.com>
2024-07-30 00:16:43 -0400

    test

--- frontend/football/src/components/PlayerDetail.jsx
+++ frontend/football/src/components/PlayerDetail.jsx
@@ -1,17 +1,9 @@
 import axios from 'axios'
 import { useState } from 'react'
 
-const PlayerDetail = () => {
+const PlayerDetail = (props) => {
 
-    const [currentUser, setCurrentUser] = useState({
-        _id: "66a79ceaa9b2ead5607bc68b",
-        username: "jslempa",
-        password: "111111",
-        email: "fakeemail1@gmail.com",
-        image: "https://placehold.co/300/green/white",
-        isLoggedIn: true,
-        balance: 100,
-    })
+    console.log('player detail props', props)
 
 
     return (
