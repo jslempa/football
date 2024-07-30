9c23e539563a375e19b1f54914085663696309c6
jslempa <jslempa@gmail.com>
2024-07-29 23:51:14 -0400

    search returns player cards that link to player detail

--- frontend/football/src/components/PlayerList.jsx
+++ frontend/football/src/components/PlayerList.jsx
@@ -1,19 +1,30 @@
 import axios from 'axios'
 import { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import DataContext from '../DataContext'
+import { Navigate } from 'react-router-dom'
 
 const PlayerList = (props) => {
 
     console.log('player list props', props)
+
+    let navigate = useNavigate()
 
     const { searchResults, setSearchResults } = useContext(DataContext) 
 
     console.log(searchResults[0])
 
     return (
-        <div>
-            <h1></h1>
-            {/* <button onClick={test}>Click me</button> */}
+        <div className='player-card'>
+            {
+                searchResults.length > 0 ? (
+                    searchResults.map((result, index) => (
+                        <div className='player-card-data' key={index} onClick={()=>navigate(`/players/${result.espnID}`)}>
+                        <h3>{result.name}</h3>
+                        </div>
+                    ))
+                ) : null
+            }          
         </div>
     )
 }
